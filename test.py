cfe2c8b441178c7ec91b8a6b4ecd55a1f3d3e53e
danXignat <dan.ignat@student.unitbv.ro>
2024-10-31 17:24:17 +0200

    laptop update

--- test.py
+++ test.py
@@ -1,23 +1,87 @@
-class Miau:
-    def __init__(self, value):
-        self.value = value
+# model.py
+class Person:
+    def __init__(self, name=""):
+        self._name = name
 
-    def __repr__(self):
-        return str(self.value)
+    @property
+    def name(self):
+        return self._name
 
-    def delete(self):
-        del self    
-        
-    
-ls = [Miau(10), Miau(20), Miau(30), Miau(40)]
+    @name.setter
+    def name(self, value):
+        self._name = value
 
-s = set(ls)
-d = {str(i): v for i, v in enumerate(ls)}
 
-print(s)
-print(d)
+# viewmodel.py
+from PySide6.QtCore import QObject, Signal
 
-d["1"].value += 700
+class PersonViewModel(QObject):
+    nameChanged = Signal(str)
 
-print(s)
-print(d)
+    def __init__(self, person):
+        super().__init__()
+        self._person = person
+
+    @property
+    def name(self):
+        return self._person.name
+
+    @name.setter
+    def name(self, value):
+        if value != self._person.name:
+            self._person.name = value
+            self.nameChanged.emit(value)
+
+
+# view.py
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit
+
+class PersonView(QWidget):
+    def __init__(self, view_model):
+        super().__init__()
+        self.view_model = view_model
+        self.init_ui()
+
+    def init_ui(self):
+        # UI Components
+        self.name_label = QLabel("Name:")
+        self.name_input = QLineEdit()
+
+        # Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.name_label)
+        layout.addWidget(self.name_input)
+        self.setLayout(layout)
+
+        # Set initial name in the input
+        self.name_input.setText(self.view_model.name)
+
+        # Connect ViewModel to View
+        self.view_model.nameChanged.connect(self.on_name_changed)
+
+        # Connect View to ViewModel
+        self.name_input.textChanged.connect(self.view_model.name_changed)
+
+    def on_name_changed(self, value):
+        # Update the input if the name changes in the ViewModel
+        self.name_input.setText(value)
+
+
+# main.py
+import sys
+from PySide6.QtWidgets import QApplication
+
+def main():
+    app = QApplication(sys.argv)
+
+    # Create Model, ViewModel, and View
+    person = Person("John Doe")
+    view_model = PersonViewModel(person)
+    view = PersonView(view_model)
+
+    # Show the main window
+    view.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
